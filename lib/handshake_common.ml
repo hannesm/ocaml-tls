0d1d8aca0bd17b6bbdccffd52c3a1cf65ce5ff3a
Hannes Mehnert <hannes@mehnert.org>
2016-01-08 23:31:54 +0100

    parse new data blobs

--- lib/handshake_common.ml
+++ lib/handshake_common.ml
@@ -85,6 +85,9 @@ let to_client_ext_type = function
   | `UnknownExtension _    -> `UnknownExtension
   | `ExtendedMasterSecret  -> `ExtendedMasterSecret
   | `ALPN _                -> `ALPN
+  | `KeyShare _            -> `KeyShare
+  | `EarlyDataIndication _ -> `EarlyDataIndication
+  | `PreSharedKey _        -> `PreSharedKey
 
 let to_server_ext_type = function
   | `Hostname              -> `Hostname
@@ -94,6 +97,9 @@ let to_server_ext_type = function
   | `UnknownExtension _    -> `UnknownExtension
   | `ExtendedMasterSecret  -> `ExtendedMasterSecret
   | `ALPN _                -> `ALPN
+  | `KeyShare _            -> `KeyShare
+  | `EarlyDataIndication   -> `EarlyDataIndication
+  | `PreSharedKey _        -> `PreSharedKey
 
 let extension_types t exts = List.(
   exts |> map t
@@ -151,7 +157,8 @@ let client_hello_valid ch =
             ch.extensions in
         not has_sig_algo )
 
-let server_hello_valid sh =
+let server_hello_valid (sh : server_hello) =
+  let open Ciphersuite in
   List_set.is_proper_set (extension_types to_server_ext_type sh.extensions)
   (* TODO:
       - EC stuff must be present if EC ciphersuite chosen
