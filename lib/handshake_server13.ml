dce1efcac49db03c37d530baf087c9c394c4ddfb
Hannes Mehnert <hannes@mehnert.org>
2018-11-28 21:49:20 +0100

    cleanup

--- lib/handshake_server13.ml
+++ lib/handshake_server13.ml
@@ -79,124 +79,29 @@ let answer_client_hello state ch raw =
       - TODO: what if KS found, but not part of supportedgroup?
       - what is PSK + KS + SG found, but PSK does not match --> (EC)DHE *)
   match
-    resumed_session,
     first_match groups state.config.Config.groups,
     first_match ciphers state.config.Config.ciphers13
   with
-  | Some epoch, Some group, _ -> invalid_arg "NYI resumed"
-(*    (* DHE_PSK *)
-    (* trace_cipher dhe_psk_cipher ; *)
-    let keyshare = keyshare group in
-    let secret, my_share = Nocrypto.Dh.gen_key group in
+  | None, _ -> fail (`Fatal `NoSupportedGroup)
+  | _, None -> fail (`Error (`NoConfiguredCiphersuite ciphers))
+  | Some group, Some cipher ->
+    match resumed_session with
+    | Some _ -> invalid_arg "PSK"
+    | None ->
+      (* DHE - full handshake *)
+      let log = match map_find ~f:(function `Cookie c -> Some c | _ -> None) ch.extensions with
+        | None -> Cstruct.empty
+        | Some c ->
+          (* log is: 254 00 00 length c :: HRR *)
+          let cs = Cstruct.create 4 in
+          Cstruct.set_uint8 cs 0 (Packet.handshake_type_to_int Packet.MESSAGE_HASH) ;
+          Cstruct.set_uint8 cs 3 (Cstruct.len c) ;
+          let hrr = { retry_version = TLS_1_3 ; ciphersuite = cipher ; selected_group = group ; extensions = [ `Cookie c ]} in
+          let hs_buf = Writer.assemble_handshake (HelloRetryRequest hrr) in
+          Cstruct.concat [ cs ; c ; hs_buf ]
+      in
 
-    let sh, session = base_server_hello ~epoch `DHE_PSK dhe_psk_cipher [`PreSharedKey epoch.psk_id ; `KeyShare (group, my_share)] in
-    let sh_raw = Writer.assemble_handshake (ServerHello sh) in
-
-    Tracing.sexpf ~tag:"handshake-out" ~f:sexp_of_tls_handshake (ServerHello sh) ;
-
-    let ss = epoch.resumption_secret in
-    ( match Nocrypto.Dh.shared group secret keyshare with
-      | None -> fail (`Fatal `InvalidDH)
-      | Some shared -> return shared ) >>= fun es ->
-
-    let log = log <+> raw <+> sh_raw in
-    let server_ctx, client_ctx = hs_ctx dhe_psk_cipher log es in
-
-    let ee = EncryptedExtensions [`Hostname] in
-    let ee_raw = Writer.assemble_handshake ee in
-
-    Tracing.sexpf ~tag:"handshake-out" ~f:sexp_of_tls_handshake ee ;
-
-    let log = log <+> ee_raw in
-    let master_secret = master_secret dhe_psk_cipher es ss log in
-    Tracing.cs ~tag:"master-secret" master_secret ;
-
-    let traffic_secret = traffic_secret dhe_psk_cipher master_secret log in
-
-    let f_data = finished dhe_psk_cipher master_secret true log in
-    let fin = Finished f_data in
-    let fin_raw = Writer.assemble_handshake fin in
-
-    Tracing.sexpf ~tag:"handshake-out" ~f:sexp_of_tls_handshake fin ;
-
-    let log = log <+> fin_raw in
-    let server_app_ctx, client_app_ctx = app_ctx dhe_psk_cipher log traffic_secret in
-
-    guard (Cs.null state.hs_fragment) (`Fatal `HandshakeFragmentsNotEmpty) >|= fun () ->
-
-    let session = { session with master_secret } in
-    let st = AwaitClientFinished13 (session, Some client_app_ctx, log) in
-    ({ state with machina = Server13 st },
-     [ `Record (Packet.HANDSHAKE, sh_raw) ;
-       `Change_enc (Some server_ctx) ;
-       `Change_dec (Some client_ctx) ;
-       `Record (Packet.HANDSHAKE, ee_raw) ;
-       `Record (Packet.HANDSHAKE, fin_raw) ;
-       `Change_enc (Some server_app_ctx) ] ) *)
-
-  | Some epoch, None, _ -> invalid_arg "NYI epoch no group"
-(*    (* PSK *)
-    trace_cipher psk_cipher ;
-    let sh, session = base_server_hello ~epoch `PSK psk_cipher [`PreSharedKey epoch.psk_id] in
-    let sh_raw = Writer.assemble_handshake (ServerHello sh) in
-
-    Tracing.sexpf ~tag:"handshake-out" ~f:sexp_of_tls_handshake (ServerHello sh) ;
-
-    let es = epoch.resumption_secret in
-    let ss = es in
-
-    let log = log <+> raw <+> sh_raw in
-    let server_ctx, client_ctx = hs_ctx psk_cipher log es in
-
-    let ee = EncryptedExtensions [`Hostname] in
-    let ee_raw = Writer.assemble_handshake ee in
-
-    Tracing.sexpf ~tag:"handshake-out" ~f:sexp_of_tls_handshake ee ;
-
-    let log = log <+> ee_raw in
-    let master_secret = master_secret psk_cipher es ss log in
-    Tracing.cs ~tag:"master-secret" master_secret ;
-
-    let traffic_secret = traffic_secret psk_cipher master_secret log in
-
-    let f_data = finished psk_cipher master_secret true log in
-    let fin = Finished f_data in
-    let fin_raw = Writer.assemble_handshake fin in
-
-    Tracing.sexpf ~tag:"handshake-out" ~f:sexp_of_tls_handshake fin ;
-
-    let log = log <+> fin_raw in
-    let server_app_ctx, client_app_ctx = app_ctx psk_cipher log traffic_secret in
-
-    guard (Cs.null state.hs_fragment) (`Fatal `HandshakeFragmentsNotEmpty) >|= fun () ->
-
-    let session = { session with master_secret } in
-    let st = AwaitClientFinished13 (session, Some client_app_ctx, log) in
-    ({ state with machina = Server13 st },
-     [ `Record (Packet.HANDSHAKE, sh_raw) ;
-       `Change_enc (Some server_ctx) ;
-       `Change_dec (Some client_ctx) ;
-       `Record (Packet.HANDSHAKE, ee_raw) ;
-       `Record (Packet.HANDSHAKE, fin_raw) ;
-       `Change_enc (Some server_app_ctx) ] ) *)
-
-  | None, Some group, Some cipher ->
-    (* DHE - full handshake *)
-
-    let log = match map_find ~f:(function `Cookie c -> Some c | _ -> None) ch.extensions with
-      | None -> Cstruct.empty
-      | Some c ->
-        (* log is: 254 00 00 length c :: HRR *)
-        let cs = Cstruct.create 4 in
-        Cstruct.set_uint8 cs 0 (Packet.handshake_type_to_int Packet.MESSAGE_HASH) ;
-        Cstruct.set_uint8 cs 3 (Cstruct.len c) ;
-        let hrr = { retry_version = TLS_1_3 ; ciphersuite = cipher ; selected_group = group ; extensions = [ `Cookie c ]} in
-        let hs_buf = Writer.assemble_handshake (HelloRetryRequest hrr) in
-        Cstruct.concat [ cs ; c ; hs_buf ]
-    in
-
-    (* trace_cipher cipher ; *)
-    begin
+      (* trace_cipher cipher ; *)
       match keyshare group with
       | None ->
         let cookie = Nocrypto.Hash.digest (Ciphersuite.hash13 cipher) raw in
@@ -206,115 +111,86 @@ let answer_client_hello state ch raw =
         return (state, [ `Record (Packet.HANDSHAKE, hrr_raw) ])
       | Some keyshare ->
         (* XXX: for-each ciphers there should be a suitable group (skipping for now since we only have DHE) *)
-    (* XXX: check sig_algs for signatures in certificate chain *)
+        (* XXX: check sig_algs for signatures in certificate chain *)
 
-    let early_secret = Handshake_crypto13.(derive (empty cipher) (Cstruct.create 32)) in
+        let early_secret = Handshake_crypto13.(derive (empty cipher) (Cstruct.create 32)) in
 
-    (* if acceptable, do server hello *)
-    let secret, public = Handshake_crypto13.dh_gen_key group in
-    (match Handshake_crypto13.dh_shared group secret keyshare with
-     | None -> fail (`Fatal `InvalidDH)
-     | Some shared -> return shared) >>= fun es ->
-    let hs_secret = Handshake_crypto13.derive early_secret es in
+        (* if acceptable, do server hello *)
+        let secret, public = Handshake_crypto13.dh_gen_key group in
+        (match Handshake_crypto13.dh_shared group secret keyshare with
+         | None -> fail (`Fatal `InvalidDH)
+         | Some shared -> return shared) >>= fun es ->
+        let hs_secret = Handshake_crypto13.derive early_secret es in
 
-    let sh, session = base_server_hello `DHE_RSA cipher [`KeyShare (group, public)] in
-    let sh_raw = Writer.assemble_handshake (ServerHello sh) in
+        let sh, session = base_server_hello `DHE_RSA cipher [`KeyShare (group, public)] in
+        let sh_raw = Writer.assemble_handshake (ServerHello sh) in
 
-    Tracing.sexpf ~tag:"handshake-out" ~f:sexp_of_tls_handshake (ServerHello sh) ;
+        Tracing.sexpf ~tag:"handshake-out" ~f:sexp_of_tls_handshake (ServerHello sh) ;
 
-    let log = log <+> raw <+> sh_raw in
-    let server_hs_secret, server_ctx, client_hs_secret, client_ctx = hs_ctx hs_secret log in
+        let log = log <+> raw <+> sh_raw in
+        let server_hs_secret, server_ctx, client_hs_secret, client_ctx = hs_ctx hs_secret log in
 
-    (* ONLY if client sent a `Hostname *)
-    let ee = EncryptedExtensions [ `Hostname ] in
-    (* TODO also max_fragment_length ; client_certificate_url ; trusted_ca_keys ; user_mapping ; client_authz ; server_authz ; cert_type ; use_srtp ; heartbeat ; alpn ; status_request_v2 ; signed_cert_timestamp ; client_cert_type ; server_cert_type *)
-    let ee_raw = Writer.assemble_handshake ee in
+        (* ONLY if client sent a `Hostname *)
+        let ee = EncryptedExtensions [ `Hostname ] in
+        (* TODO also max_fragment_length ; client_certificate_url ; trusted_ca_keys ; user_mapping ; client_authz ; server_authz ; cert_type ; use_srtp ; heartbeat ; alpn ; status_request_v2 ; signed_cert_timestamp ; client_cert_type ; server_cert_type *)
+        let ee_raw = Writer.assemble_handshake ee in
 
-    Tracing.sexpf ~tag:"handshake-out" ~f:sexp_of_tls_handshake ee ;
+        Tracing.sexpf ~tag:"handshake-out" ~f:sexp_of_tls_handshake ee ;
 
-    let crt, pr = match state.config.Config.own_certificates with
-      | `Single (chain, priv) -> chain, priv
-      | _ -> assert false
-    in
-    let certs = List.map X509.Encoding.cs_of_cert crt in
-    let cert = Certificate (Writer.assemble_certificates_1_3 Cstruct.empty certs) in
-    let cert_raw = Writer.assemble_handshake cert in
-
-    Tracing.sexpf ~tag:"handshake-out" ~f:sexp_of_tls_handshake cert ;
-
-    let log = Cstruct.concat [ log ; ee_raw ; cert_raw ] in
-    signature TLS_1_3 ~context_string:"TLS 1.3, server CertificateVerify"
-      log (Some sigalgs) state.config.Config.signature_algorithms pr >>= fun signed ->
-    let cv = CertificateVerify signed in
-    let cv_raw = Writer.assemble_handshake cv in
-
-    Tracing.cs ~tag:"cv" cv_raw ;
-    Tracing.sexpf ~tag:"handshake-out" ~f:sexp_of_tls_handshake cv ;
-
-    let log = log <+> cv_raw in
-    let master_secret = Handshake_crypto13.derive hs_secret (Cstruct.create 32) in
-    Tracing.cs ~tag:"master-secret" master_secret.secret ;
-    (* let resumption_secret = resumption_secret cipher master_secret log in *)
-
-    let f_data = finished hs_secret.hash server_hs_secret log in
-    let fin = Finished f_data in
-    let fin_raw = Writer.assemble_handshake fin in
-
-    Tracing.sexpf ~tag:"handshake-out" ~f:sexp_of_tls_handshake fin ;
-
-    let log = log <+> fin_raw in
-    let _, server_app_ctx, _, client_app_ctx = app_ctx master_secret log in
-
-    guard (Cs.null state.hs_fragment) (`Fatal `HandshakeFragmentsNotEmpty) >|= fun () ->
-
-    let session =
-      let common_session_data13 = {
-        session.common_session_data13 with
-        own_private_key = Some pr ;
-        own_certificate = crt ;
-        master_secret = master_secret.secret
-      } in
-      { session with common_session_data13 (* TODO ; resumption_secret ; exporter_secret *) } in
-    (* new state: one of AwaitClientCertificate13 , AwaitClientFinished13 *)
-    let st = AwaitClientFinished13 (session, client_hs_secret, client_app_ctx, log) in
-    ({ state with machina = Server13 st },
-     [ `Record (Packet.HANDSHAKE, sh_raw) ;
-       `Change_enc (Some server_ctx) ;
-       `Change_dec (Some client_ctx) ;
-       `Record (Packet.HANDSHAKE, ee_raw) ;
-       `Record (Packet.HANDSHAKE, cert_raw) ;
-       `Record (Packet.HANDSHAKE, cv_raw) ;
-       `Record (Packet.HANDSHAKE, fin_raw) ;
-       `Change_enc (Some server_app_ctx) ] )
-    end
-  | None, None, None
-  | None, Some _, None
-  | None, None, Some _ -> invalid_arg "NYI other"
-
-(*  | _, None, _, _, Some cipher when Cs.null log ->
-    ( match first_match groups state.config.Config.groups with
-      | None -> fail (`Fatal `NoSupportedGroup)
-      | Some group ->
-        let hrr = { version = TLS_1_3 ;
-                    ciphersuite = cipher ;
-                    selected_group = group ;
-                    extensions = [] }
+        let crt, pr = match state.config.Config.own_certificates with
+          | `Single (chain, priv) -> chain, priv
+          | _ -> assert false
         in
-        let hrr_raw = Writer.assemble_handshake (HelloRetryRequest hrr) in
+        let certs = List.map X509.Encoding.cs_of_cert crt in
+        let cert = Certificate (Writer.assemble_certificates_1_3 Cstruct.empty certs) in
+        let cert_raw = Writer.assemble_handshake cert in
 
-        Tracing.sexpf ~tag:"handshake-out" ~f:sexp_of_tls_handshake (HelloRetryRequest hrr) ;
+        Tracing.sexpf ~tag:"handshake-out" ~f:sexp_of_tls_handshake cert ;
 
-        let log = raw <+> hrr_raw in
-        let st = AwaitClientHello13 (ch, hrr, log) in
-        return ({ state with machina = Server13 st },
-                [ `Record (Packet.HANDSHAKE, hrr_raw) ]) )
+        let log = Cstruct.concat [ log ; ee_raw ; cert_raw ] in
+        signature TLS_1_3 ~context_string:"TLS 1.3, server CertificateVerify"
+          log (Some sigalgs) state.config.Config.signature_algorithms pr >>= fun signed ->
+        let cv = CertificateVerify signed in
+        let cv_raw = Writer.assemble_handshake cv in
 
-  | _, _, _, _, Some _ ->
-    (* already sent a hello retry request, not doing this game again *)
-    fail (`Fatal `InvalidMessage)
+        Tracing.cs ~tag:"cv" cv_raw ;
+        Tracing.sexpf ~tag:"handshake-out" ~f:sexp_of_tls_handshake cv ;
 
-  | _, _, _, _, None ->
-    fail (`Error (`NoConfiguredCiphersuite ciphers)) *)
+        let log = log <+> cv_raw in
+        let master_secret = Handshake_crypto13.derive hs_secret (Cstruct.create 32) in
+        Tracing.cs ~tag:"master-secret" master_secret.secret ;
+        (* let resumption_secret = resumption_secret cipher master_secret log in *)
+
+        let f_data = finished hs_secret.hash server_hs_secret log in
+        let fin = Finished f_data in
+        let fin_raw = Writer.assemble_handshake fin in
+
+        Tracing.sexpf ~tag:"handshake-out" ~f:sexp_of_tls_handshake fin ;
+
+        let log = log <+> fin_raw in
+        let _, server_app_ctx, _, client_app_ctx = app_ctx master_secret log in
+
+        guard (Cs.null state.hs_fragment) (`Fatal `HandshakeFragmentsNotEmpty) >|= fun () ->
+
+        let session =
+          let common_session_data13 = {
+            session.common_session_data13 with
+            own_private_key = Some pr ;
+            own_certificate = crt ;
+            master_secret = master_secret.secret
+          } in
+          { session with common_session_data13 (* TODO ; resumption_secret ; exporter_secret *) } in
+        (* new state: one of AwaitClientCertificate13 , AwaitClientFinished13 *)
+        let st = AwaitClientFinished13 (session, client_hs_secret, client_app_ctx, log) in
+        ({ state with machina = Server13 st },
+         [ `Record (Packet.HANDSHAKE, sh_raw) ;
+           `Change_enc (Some server_ctx) ;
+           `Change_dec (Some client_ctx) ;
+           `Record (Packet.HANDSHAKE, ee_raw) ;
+           `Record (Packet.HANDSHAKE, cert_raw) ;
+           `Record (Packet.HANDSHAKE, cv_raw) ;
+           `Record (Packet.HANDSHAKE, fin_raw) ;
+           `Change_enc (Some server_app_ctx) ] )
 
 let answer_client_finished state fin (sd : session_data13) client_fini dec_ctx raw log =
   let hash = Ciphersuite.hash13 sd.ciphersuite13 in
